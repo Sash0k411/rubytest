26a4731b42c6b8d52986d3a89ae99232f79a0a8d
Your Name <you@example.com>
2022-04-14 17:06:13 +0300

    Bootstrap mod

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,11 +2,4 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
-environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-        $: 'jquery',
-        JQuery: 'jquery',
-        Popper: ['popper.js', 'default'], // for Bootstrap 4
-    })
-)
-
 module.exports = environment
