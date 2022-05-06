a037158aeb332fc2fffdaeda97dfd6ac3d88bf5a
Your Name <you@example.com>
2022-05-06 20:01:46 +0300

    Namespace updated

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,5 @@
 module Admin
   class OrdersController < ApplicationController
-    before_action :set_cart, only: [:new, :create]
     before_action :ensure_cart_isnt_empty, only: :new
     before_action :set_order, only: [:show, :edit, :update, :destroy]
 
